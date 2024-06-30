ff3f309737f219d234f848fb011cfae42d555ff2
nkowne63rt <neutron63zf@gmail.com>
2024-06-30 16:21:45 +0900

    add: full epoch

--- main.py
+++ main.py
@@ -353,10 +353,11 @@ def eval(model, dataloader, optimizer, criterion, device):
 
         total_loss += loss.item()
         total_acc += VQA_criterion(pred.argmax(1), answers)  # VQA accuracy
-        simple_acc += (pred.argmax(1) == mode_answer).mean().item()  # simple accuracy
+        simple_acc += (pred.argmax(1) == mode_answer).float().mean().item()  # simple accuracy
 
     return total_loss / len(dataloader), total_acc / len(dataloader), simple_acc / len(dataloader), time.time() - start
 
+from torch.utils.data.dataset import Subset
 
 def main():
     # deviceの設定
@@ -368,25 +369,35 @@ def main():
         transforms.Resize((224, 224)),
         transforms.ToTensor()
     ])
-    train_dataset = VQADataset(df_path="./data/train.json", image_dir="./data/train", transform=transform)
+    all_dataset = VQADataset(df_path="./data/train.json", image_dir="./data/train", transform=transform)
     test_dataset = VQADataset(df_path="./data/valid.json", image_dir="./data/valid", transform=transform, answer=False)
-    test_dataset.update_dict(train_dataset)
+    test_dataset.update_dict(all_dataset)
+
+    n_samples = len(all_dataset)
+    train_size = int(n_samples * 0.8)
+
+    train_indices = list(range(0,train_size))
+    valid_indices = list(range(train_size, n_samples))
+
+    train_dataset = Subset(all_dataset, train_indices)
+    valid_dataset = Subset(all_dataset, valid_indices)
 
     train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=128, shuffle=True)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=128, shuffle=True)
     test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False)
 
-    model = VQAModel(vocab_size=len(train_dataset.question2idx)+1, n_answer=len(train_dataset.answer2idx)).to(device)
+    model = VQAModel(vocab_size=len(all_dataset.question2idx)+1, n_answer=len(all_dataset.answer2idx)).to(device)
 
     # optimizer / criterion
     # num_epoch = 20
-    num_epoch = 1
+    num_epoch = 50
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 
     # train model
     for epoch in range(num_epoch):
         train_loss, train_acc, train_simple_acc, train_time = train(model, train_loader, optimizer, criterion, device)
-        valid_loss, valid_acc, valid_simple_acc, valid_time = eval(model, test_loader, optimizer, criterion, device)
+        valid_loss, valid_acc, valid_simple_acc, valid_time = eval(model, valid_loader, optimizer, criterion, device)
         print(f"【{epoch + 1}/{num_epoch}】\n"
               f"train time: {train_time:.2f} [s]\n"
               f"train loss: {train_loss:.4f}\n"
@@ -406,7 +417,7 @@ def main():
         pred = pred.argmax(1).cpu().item()
         submission.append(pred)
 
-    submission = [train_dataset.idx2answer[id] for id in submission]
+    submission = [all_dataset.idx2answer[id] for id in submission]
     submission = np.array(submission)
     torch.save(model.state_dict(), "model.pth")
     np.save("submission.npy", submission)
